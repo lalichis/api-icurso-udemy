a1a5944fdfd95baa5e60a47a816c4e36084bb256
lalichis <eduardorochaviveros@gmail.com>
2023-05-14 22:48:50 -0600

    Mejora para cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const incident_router = require("./routes/incidents")
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 module.exports = app;
