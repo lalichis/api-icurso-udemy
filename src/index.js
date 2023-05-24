5b7def93e17753cafc6e61728410c09c56593284
lalichis <eduardorochaviveros@gmail.com>
2023-05-23 22:16:21 -0600

    230523_cadena de conexion mejorada

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const app = require("./app");
+
+require('dotenv').config();
+
+const { CONNECTION_STRING } = process.env;
+
+
 const mongoose = require("mongoose");
 const port = 4000;
-const urlMongoDB="mongodb+srv://dbaUser:dbaUser@cluster0.4k13s.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect('mongodb+srv://dbaUser:dbaUser@cluster0.4k13s.mongodb.net/?retryWrites=true&w=majority',{
+
+
+//mongoose.connect('mongodb+srv://dbaUser:dbaUser@cluster0.4k13s.mongodb.net/?retryWrites=true&w=majority',{
+    
+mongoose.connect(CONNECTION_STRING,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
@@ -12,8 +21,10 @@ mongoose.connect('mongodb+srv://dbaUser:dbaUser@cluster0.4k13s.mongodb.net/?retr
 .catch(err => console.error(err));
 
 
+
+
 app.listen(port, ()=>{
-    console.log("Server +++ running at http://localhost:"+port);
+    console.log("Server +++ running at http://localhost:" + port);
 
 });
 
